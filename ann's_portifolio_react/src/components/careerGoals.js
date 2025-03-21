d848ddb5860bd24cdf67412ed91f65de8e120e07
a-kum2580 <132387920+a-kum2580@users.noreply.github.com>
2025-03-21 21:47:50 +0300

    .

---
+++ ann's_portifolio_react/src/components/careerGoals.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function CareerGoal() {
+    return(
+      <>
+      <h2>CAREER GOALS</h2>
+          <div className="career-goals">
+            <p>
+            My primary career goal is to become an Embedded Systems and Robotics Engineer, specializing in the integration of AI and IoT to develop smart and autonomous systems. I am particularly interested in working with emerging technologies like artificial intelligence, blockchain, and edge computing to create innovative solutions in automation and smart devices.
+            </p>
+            <p>
+            In the short term, I aim to secure an internship at a leading tech company to gain hands-on experience in hardware-software integration, microcontroller programming, and real-time systems. Long-term, I aspire to establish my own technology consultancy firm, focusing on sustainable and ethical innovations in embedded systems, robotics, and AI-driven automation.
+            </p>
+          </div>
+          </>
+    )
+    
+  }
+
+  export default CareerGoal;
